47aeac7b20fd3538fc7a039abb6a94033aaf8606
GornushkinAP <gapedu@yandex.ru>
2024-01-29 22:35:11 +0300

    сделал первую таску

---
+++ sem21.01.24/Task1SRP/empInfoPrint.java
@@ -0,0 +1,7 @@
+package Task1SRP;
+
+public class empInfoPrint {
+    public String getEmployeeInfo(Employee employee) {
+        return employee.getEmpInfo();
+    }
+}
---
+++ sem21.01.24/Task1SRP/salaryCalculator.java
@@ -0,0 +1,7 @@
+package Task1SRP;
+
+public class salaryCalculator {
+    public int calculateNetSalary(Employee employee) {
+        return employee.calculateNetSalary();
+    }
+}
